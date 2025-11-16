3008c332b0a711ae8077b0eca6a9940e5b96e1a0
rahu-oss <rk0254264@gmail.com>
2025-11-16 15:08:50 +0530

    Refactor index.js for cleaner code and updates

--- index.js
+++ index.js
@@ -1,87 +1,78 @@
-const express= require("express");
-const app= express();
-const mongoose = require('mongoose');
-const path= require('path');
-const Chat= require("./models/chart.js");
-const methodOverride= require("method-override");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chart.js");
+const methodOverride = require("method-override");
+
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
-
+// Database Connection
 main()
-    .then(()=>{
-        console.log("connection successful");
-    })
+    .then(()=> console.log("DB connected"))
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-
-
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
-//Index Route
+// Index Route
 app.get("/chats", async(req,res)=>{
-    let chats= await Chat.find();
-    res.render("index.ejs", {chats});
-})
-
-//New Route
-app.get("/chats/new", (req, res)=>{
-    res.render("new.ejs");
-})
-
-app.post("/chats",(req,res)=>{
-    let {from,to,msg}= req.body;
-    let newChat= new Chat({
-        from:from,
-        to:to,
-        msg:msg,
-        created_at: new Date()
-    });
-    newChat
-        .save()
-        .then((res)=>{
-            console.log("chat save");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    res.redirect("/chats");
-    
+    let chats = await Chat.find();
+    res.render("index", {chats});
 });
 
-app.get("/chats/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-    let chat= await Chat.findById(id);
-    res.render("edit",{chat})
-})
+// New form
+app.get("/chats/new", (req,res)=>{
+    res.render("new");
+});
 
-//Update Route
-app.put("/chats/:id", async(req,res)=>{
-    let {id}=req.params;
-    let {msg:newMsg}=req.body;
-    let updateChat=await Chat.findByIdAndUpdate(id,
-        {msg:newMsg},
-        {runValidators:true,new:true}
-    )
-    res.redirect("/chats")
-})
-
-//Delete Route
-app.delete("/chats/:id", async(req,res)=>{
-    let {id}=req.params;
-    let deletedChat= await Chat.findByIdAndDelete(id);
+// Create Route
+app.post("/chats",(req,res)=>{
+    let {from,to,msg} = req.body;
+    let newChat = new Chat({
+        from,
+        to,
+        msg,
+        created_at: new Date()
+    });
+    newChat.save().then(()=> console.log("chat saved"))
+                  .catch(err=> console.log(err));
     res.redirect("/chats");
-})
+});
 
+// Edit Route
+app.get("/chats/:id/edit", async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit", {chat});
+});
 
-app.get('/',(req,res)=>{
-    res.send("working");
-})
+// Update Route
+app.put("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    let {msg:newMsg} = req.body;
+    await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
+    res.redirect("/chats");
+});
 
-app.listen(8080, ()=>{
-    console.log("server is listining on port");
-})
+// Delete
+app.delete("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+});
+
+app.get("/", (req,res)=>{
+    res.send("App Working!");
+});
+
+// PORT FIX for Render
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("Server is running...");
+});
+
